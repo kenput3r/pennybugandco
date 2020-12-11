90c98e61e70bf4fdcc18519a12e8fb429188bc0d
Kenneth Lyerly <kenput3r@gmail.com>
2020-12-11 09:49:58 -0800

    remove await

--- functions/log_order-background.js
+++ functions/log_order-background.js
@@ -46,7 +46,7 @@ async function main(order) {
   // })
   console.log(transporter)
   // send mail with defined transport object
-  let info = await transporter.sendMail(
+  transporter.sendMail(
     {
       from: '"Penny Bug & Co 🐾 " <info@pennybug.co>', // sender address
       to: `info@pennybug.co, ${customer_email}`, // list of receivers
@@ -73,14 +73,12 @@ async function main(order) {
     }
   )
 
-  console.log("info", info)
+  // console.log("Message sent: %s", info.messageId)
+  // // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  console.log("Message sent: %s", info.messageId)
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  // // Preview only available when sending through an Ethereal account
+  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
+  // // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
 exports.handler = async function (event, context) {
